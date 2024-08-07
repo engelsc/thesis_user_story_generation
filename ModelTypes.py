173b4f15b5c886ab0240c0ab467face1dc8297e1
engelsc <engelsc8@gmail.com>
2024-08-07 17:23:47 +0200

    Initial commit

---
+++ ModelTypes.py
@@ -0,0 +1,144 @@
+from abc import abstractmethod, ABC
+from enum import Enum
+from typing import Final, override, cast
+from os import getenv
+import pandas as pd
+import random
+from openai import AsyncOpenAI
+
+# import json
+import asyncio
+
+
+# Retrieve the API key from the environment variable
+OPENROUTER_API_KEY = getenv("OPEN_ROUTER_API_KEY")
+if OPENROUTER_API_KEY is None:
+    raise ValueError(
+        "OpenRouter API key not found. Please set the OPEN_ROUTER_API_KEY environment variable."
+    )
+
+
+# Enum for all models available
+class MODEL_TYPES(Enum):
+    TEST_IMPLEMENTATION = "test_implementation"
+    GPT = "gpt"
+    CLAUDE = "claude"
+    GEMINI = "gemini"
+    LLAMA = "llama"
+    MISTRAL = "mistral"
+    MIXTRAL = "mixtral"
+
+    @staticmethod
+    def is_member(member_string: str) -> bool:
+        return member_string in MODEL_TYPES._value2member_map_
+
+
+class ModelType(ABC):
+    # MODEL_ID: Final[MODEL_TYPES]
+
+    def __init__(self, model_id: str) -> None:
+        if MODEL_TYPES.is_member(model_id):
+            self.MODEL_ID: Final[MODEL_TYPES] = MODEL_TYPES(model_id)
+        else:
+            raise ValueError(f"'{model_id}' is not a valid MODEL_TYPE")
+
+    def get_id(self) -> str:
+        return self.MODEL_ID.value
+
+    def create_final_prompts(
+        self, data_set: pd.DataFrame, model_prompt: str
+    ) -> list[str]:
+        final_prompts: list[str] = []
+        for idx, _ in data_set.iterrows():
+            idx = cast(int, idx)
+            final_prompts.append(
+                model_prompt
+                + "\n\nREQUIREMENT:\n"
+                + str(data_set.iloc[idx]["text_description"])
+            )
+        # print("Final prompts: " + str(final_prompts))
+        return final_prompts
+
+    @abstractmethod
+    async def generate_responses(
+        self, raw_data: pd.DataFrame, model_prompt: str, run_amount: int
+    ) -> pd.DataFrame:
+        pass
+
+
+class Llama31Free(ModelType):
+    # Free model provided on OpenRouter with openrouter api
+    def __init__(self) -> None:
+        super().__init__("llama")
+
+    @override
+    async def generate_responses(
+        self, raw_data: pd.DataFrame, model_prompt: str, run_amount: int
+    ) -> pd.DataFrame:
+        async with AsyncOpenAI(
+            base_url="https://openrouter.ai/api/v1", api_key=OPENROUTER_API_KEY
+        ) as client:
+            final_prompts = self.create_final_prompts(raw_data, model_prompt)
+            # TODO: include run_amount into call for responses -> run_amount of stories per datapoint
+
+            operations = [self.post_request(prompt, client) for prompt in final_prompts]
+            responses: list[str] = await asyncio.gather(*operations)
+
+        # TODO: enrich response data with data from responses for each created story for run_amount
+        response_data = raw_data.copy()
+        for id, _ in response_data.iterrows():
+            id = cast(int, id)
+            response_data.at[id, "story_1"] = responses[id]
+
+        # print("LLM RESPONSES:\n" + str(responses))
+        return response_data
+
+    async def post_request(self, prompt: str, client: AsyncOpenAI) -> str:
+        completion = await client.chat.completions.create(
+            model="mistralai/mistral-7b-instruct:free",
+            messages=[
+                {
+                    "role": "user",
+                    "content": prompt,
+                },
+            ],
+        )
+        return str(completion.choices[0].message.content)
+
+
+# TESTIMPLEMENNTATION
+class TestImplementation(ModelType):
+    user_stories = [
+        "As a customer, I want to filter products by price range, brand, and customer ratings so that I can find the products that best match my preferences and budget.",
+        "As a social media user, I want to update my profile picture and bio so that my profile accurately reflects my current appearance and interests.",
+        "As a patient, I want to book, reschedule, or cancel my medical appointments online so that I can manage my healthcare schedule conveniently.",
+        "As a fitness enthusiast, I want my fitness tracker data to sync automatically with my health app so that I can track my workouts and progress in one place.",
+        "As a home cook, I want to share my recipes with a community of food enthusiasts so that others can try my dishes and provide feedback.",
+        "As an event organizer, I want to create and manage events, send invitations, and track RSVPs so that I can ensure the event runs smoothly and attendees are informed.",
+        "As a student, I want to track my progress and receive a certificate upon course completion so that I can showcase my achievements and knowledge gained.",
+        "As a bank customer, I want to receive notifications for transactions and low balance alerts so that I can monitor my account activity and avoid overdrafts.",
+        "As a team member, I want to assign tasks, set deadlines, and track progress in a shared workspace so that our team can collaborate effectively and meet project goals.",
+        "As a movie lover, I want to create and manage a watchlist and receive recommendations so that I can easily find and enjoy new content that matches my interests.",
+    ]
+
+    def __init__(self) -> None:
+        super().__init__("test_implementation")
+
+    @override
+    async def generate_responses(
+        self, raw_data: pd.DataFrame, model_prompt: str, run_amount: int
+    ) -> pd.DataFrame:
+        if run_amount < 1 or run_amount > 3:
+            raise ValueError("The value for run_amount must be between 1 and 3!")
+
+        response_data = raw_data.copy()
+
+        for id, _ in response_data.iterrows():
+            if run_amount >= 1:
+                response_data.at[id, "story_1"] = random.choice(self.user_stories)
+            if run_amount >= 2:
+                response_data.at[id, "story_2"] = random.choice(self.user_stories)
+            if run_amount == 3:
+                response_data.at[id, "story_3"] = random.choice(self.user_stories)
+
+        return response_data
---
+++ _openrouter_api_connection_test.py
@@ -0,0 +1,32 @@
+## DEPRECATED!
+
+# %% run everything
+from ModelTypes import Llama31Free
+import data_helper as dh
+import pandas as pd
+import aqusahandler
+
+# %% run second
+model = Llama31Free()
+generated_stories_count: int = 1
+
+raw_requirements = dh.load_requirements("dummy_requirements.csv")
+user_prompt: str = dh.load_prompt(model.get_id(), "prompts.csv")
+
+single_requirement: str = raw_requirements.iloc[4]["text_description"]
+
+# build prompt
+
+full_prompt = user_prompt + "\n\nREQUIREMENT:\n" + single_requirement
+print(full_prompt)
+
+# %% hand over prompt to model
+
+response: str = model.post_request(full_prompt)
+print(response)
+
+# %%
+with open("input/user_stories.txt", "w") as file:
+    file.write(response)
+
+aqusahandler.run_aqusacore("user_stories.txt", "user_stories_evaluated", "txt")
