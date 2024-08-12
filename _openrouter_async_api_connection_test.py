7ed568b2dfda924db2b57c714cfbbd746d7efff3
lyukyui <lyukyui@gmx.de>
2024-08-12 23:52:03 +0200

    Update openrouter async test to use txt when calling aqusa

--- _openrouter_async_api_connection_test.py
+++ _openrouter_async_api_connection_test.py
@@ -7,7 +7,7 @@ import aqusahandler
 
 # %% build requisites
 model = Llama31Free()
-generated_stories_count: int = 1
+generated_stories_count: int = 2
 
 raw_requirements: pd.DataFrame = dh.load_requirements("dummy_requirements.csv")
 user_prompt: str = dh.load_prompt(model.get_id(), "prompts.csv")
@@ -19,12 +19,15 @@ requirements_subset: pd.DataFrame = raw_requirements.iloc[:2]
 
 # %% hand over prompt to model
 async def get_responses() -> pd.DataFrame:
-    response_data = await model.generate_responses(requirements_subset, user_prompt, 1)
+    response_data = await model.generate_responses(
+        requirements_subset, user_prompt, generated_stories_count
+    )
     return response_data
 
 
 # HACK: Doesn't work outside of notebook running. Comment out when not running as notebook
 response_data: pd.DataFrame = await get_responses()
+print(response_data)
 
 # %% Extract user stories into txt-fole
 aqusahandler.prepare_user_stories(
@@ -33,13 +36,21 @@ aqusahandler.prepare_user_stories(
 
 # %% hand over to AQUSA-core
 aqusahandler.run_aqusacore("user_stories.txt", "user_stories_evaluated", "html")
+aqusahandler.run_aqusacore("user_stories.txt", "user_stories_evaluated", "txt")
 
 # %% Extract story output from aqusa_core merge with requirements data and save as JSON
 
-user_stories_parsed = aqusahandler.parse_user_stories_html(
-    "output/user_stories_evaluated.html"
-)
+# user_stories_parsed = aqusahandler.parse_user_stories_html(
+#    "output/user_stories_evaluated.html"
+# )
+# print(user_stories_parsed)
 
+user_stories_parsed = aqusahandler.parse_user_stories_txt(
+    "output/user_stories_evaluated.txt"
+)
+print(user_stories_parsed)
+
+# %%
 merged_data = dh.merge_data(response_data, user_stories_parsed, generated_stories_count)
 json_data = dh.save_to_json(
     merged_data, f"merged_requirements_data_{model.get_id()}.json"
